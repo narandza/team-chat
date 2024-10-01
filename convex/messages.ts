356bb3040e9c2d04ae9d7b4c8eec42724d524547
narandza <60319154+narandza@users.noreply.github.com>
2024-10-01 15:00:22 +0200

    Messages GET API: add helper functions to messages.ts, create get api call, create getMessagesCustom hook

--- convex/messages.ts
+++ convex/messages.ts
@@ -1,8 +1,61 @@
 import { v } from "convex/values";
 
-import { QueryCtx, mutation } from "./_generated/server";
+import { QueryCtx, mutation, query } from "./_generated/server";
 import { getAuthUserId } from "@convex-dev/auth/server";
 import { Id } from "./_generated/dataModel";
+import { paginationOptsValidator } from "convex/server";
+
+const populateThread = async (ctx: QueryCtx, messageId: Id<"messages">) => {
+  const messages = await ctx.db
+    .query("messages")
+    .withIndex("by_parent_message_id", (q) =>
+      q.eq("parentMessageId", messageId)
+    )
+    .collect();
+
+  if (messages.length === 0) {
+    return {
+      count: 0,
+      image: undefined,
+      timestamp: 0,
+    };
+  }
+
+  const lastMessage = messages[messages.length - 1];
+
+  const lastMessageMember = await populateMember(ctx, lastMessage.memberId);
+
+  if (!lastMessageMember) {
+    return {
+      count: 0,
+      image: undefined,
+      timestamp: 0,
+    };
+  }
+
+  const lastMessageUser = await populateUser(ctx, lastMessageMember.userId);
+
+  return {
+    count: messages.length,
+    image: lastMessageUser?.image,
+    timestamp: lastMessage._creationTime,
+  };
+};
+
+const populateReactions = (ctx: QueryCtx, messageId: Id<"messages">) => {
+  return ctx.db
+    .query("reactions")
+    .withIndex("by_message_id", (q) => q.eq("messageId", messageId))
+    .collect();
+};
+
+const populateUser = (ctx: QueryCtx, userId: Id<"users">) => {
+  return ctx.db.get(userId);
+};
+
+const populateMember = (ctx: QueryCtx, memberId: Id<"members">) => {
+  return ctx.db.get(memberId);
+};
 
 const getMember = async (
   ctx: QueryCtx,
@@ -16,6 +69,43 @@ const getMember = async (
     )
     .unique();
 };
+
+export const get = query({
+  args: {
+    channelId: v.optional(v.id("channels")),
+    conversationId: v.optional(v.id("conversations")),
+    parentMessageId: v.optional(v.id("messages")),
+    paginationOpts: paginationOptsValidator,
+  },
+  handler: async (ctx, args) => {
+    const userId = await getAuthUserId(ctx);
+
+    if (!userId) throw new Error("Unauthorized");
+
+    let _conversation_id = args.conversationId;
+
+    if (!args.conversationId && !args.channelId && args.parentMessageId) {
+      const parentMessage = await ctx.db.get(args.parentMessageId);
+
+      if (!parentMessage) throw new Error("Parent message not found");
+
+      _conversation_id = parentMessage.conversationId;
+    }
+
+    const results = await ctx.db
+      .query("messages")
+      .withIndex("by_channel_id_parent_message_id_conversation_id", (q) =>
+        q
+          .eq("channelId", args.channelId)
+          .eq("parentMessageId", args.parentMessageId)
+          .eq("conversationId", _conversation_id)
+      )
+      .order("desc")
+      .paginate(args.paginationOpts);
+
+    return results;
+  },
+});
 
 export const create = mutation({
   args: {
--- convex/schema.ts
+++ convex/schema.ts
@@ -40,6 +40,7 @@ const schema = defineSchema({
     .index("by_workspace_id", ["workspaceId"])
     .index("by_member_id", ["memberId"])
     .index("by_channel_id", ["channelId"])
+    .index("by_parent_message_id", ["parentMessageId"])
     .index("by_conversation_id", ["conversationId"])
     .index("by_channel_id_parent_message_id_conversation_id", [
       "channelId",
