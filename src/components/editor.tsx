2affe3de2d85cd9d90b48ba446aa181af573337c
narandza <60319154+narandza@users.noreply.github.com>
2024-10-01 13:13:00 +0200

    Editor submit functionality: add messages table to schema, create messages create api call, create createMessage custom hook, implemnet submit functionallity, modify quill styling

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -83,7 +83,19 @@ const Editor = ({
             enter: {
               key: "Enter",
               handler: () => {
-                // TODO submit form
+                const text = quill.getText();
+                const addedImage = imageElementRef.current?.files?.[0] || null;
+
+                const isEmpty =
+                  !addedImage &&
+                  text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+
+                if (isEmpty) return;
+
+                const body = JSON.stringify(quill.getContents());
+
+                submitRef.current?.({ body, image: addedImage });
+
                 return;
               },
               shift_enter: {
@@ -144,7 +156,7 @@ const Editor = ({
     quill?.insertText(quill?.getSelection()?.index || 0, emoji.native);
   };
 
-  const isEmpty = text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
+  const isEmpty = !image && text.replace(/<(.|\n)*?>/g, "").trim().length === 0;
 
   return (
     <div className="flex flex-col">
@@ -155,7 +167,12 @@ const Editor = ({
         onChange={(event) => setImage(event.target.files![0])}
         className="hidden"
       />
-      <div className="flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white">
+      <div
+        className={cn(
+          "flex flex-col border border-slate-200 rounded-md overflow-hidden focus-within:border-slate-300 focus-within:shadow-sm transition bg-white",
+          disabled && "opacity-50"
+        )}
+      >
         <div className="h-full ql-custom" ref={containerRef} />
         {!!image && (
           <div className="p-2">
@@ -216,14 +233,19 @@ const Editor = ({
               <Button
                 variant="outline"
                 size="sm"
-                onClick={() => {}}
+                onClick={onCancel}
                 disabled={disabled}
               >
                 Cancel
               </Button>
               <Button
                 size="sm"
-                onClick={() => {}}
+                onClick={() => {
+                  onSubmit({
+                    body: JSON.stringify(quillRef.current?.getContents()),
+                    image,
+                  });
+                }}
                 disabled={disabled || isEmpty}
                 className="bg-[#007a5a] hover: bg-[#007a5a]/80 text-white"
               >
@@ -233,7 +255,12 @@ const Editor = ({
           )}
           {variant === "create" && (
             <Button
-              onClick={() => {}}
+              onClick={() => {
+                onSubmit({
+                  body: JSON.stringify(quillRef.current?.getContents()),
+                  image,
+                });
+              }}
               disabled={disabled || isEmpty}
               size="iconSm"
               className={cn(
